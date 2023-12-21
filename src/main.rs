cd397cecf5bef9ae05f8e4bf04c797d8b7c65e6c
jeermuce <ernesto_mc.dev@proton.me>
2023-12-20 20:35:56 -0600

    Create a program that gets the min and max sum of 4 elements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fn mini_max_sum(arr: &[i32]) {
+    // sort arr
+    let mut arr = arr.to_vec();
+    arr.sort_unstable(); // We don't care about the order of equal elements and
+                         // we are working with primitive types, so this is fine
+    let min_sum: i64 = arr
+        .iter() // Creates an iterator for the array
+        .take(4) // Takes the first 4 elements
+        .map(|&x| x as i64) //map takes a function and applies it to each element
+        .sum(); // Sums the elements, returning an i64
+    let max_sum: i64 = arr
+        .iter()
+        .skip(1) // Skips a number of elements, doesn't take index into account
+        .take(4)
+        .map(|&x| x as i64)
+        .sum();
+    println!("{} {}", min_sum, max_sum);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let arr: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    mini_max_sum(&arr);
 }
